3a06c27563c37a48d6b034a0fcc9de919cf09f88
CeleDKo <daniil-210968@yandex.ru>
2023-11-22 13:22:18 +0500

    Initial Commit

---
+++ FishTextGet/FishTextGetApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishTextGetApp.swift
+//  FishTextGet
+//
+//  Created by Viktor Maltsev on 22.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct FishTextGetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
